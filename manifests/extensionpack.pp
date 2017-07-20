60bfd9ba47a89a6c02a8862c8dd832451ceb0285
Jakov Sosic <jsosic@gmail.com>
2017-07-20 23:16:49 +0200

    Auto accept license on extpack install

--- manifests/extensionpack.pp
+++ manifests/extensionpack.pp
@@ -1,8 +1,11 @@
-# Class: virtualbox::extensionpack
+#
+# = Class: virtualbox::extensionpack
 #
 # This module installs VirtualBox Extension Pack
 #
-class virtualbox::extensionpack {
+class virtualbox::extensionpack (
+  $license = '_please_create_license_',
+) {
   require virtualbox
 
   $vboxepack="Oracle_VM_VirtualBox_Extension_Pack-${::virtualbox_version}-${::virtualbox_build}.vbox-extpack"
@@ -10,8 +13,9 @@ class virtualbox::extensionpack {
   # after the extpack is installed, vboxdrv and vboxweb-service have to be stopped and cold started
   # for extpack to start working properly again
   exec { 'install_vbox_extensionpack':
-    command => "/usr/bin/wget http://download.virtualbox.org/virtualbox/${::virtualbox_version}/${vboxepack} && \
-                /usr/bin/VBoxManage extpack install --replace ${vboxepack} && /bin/rm -f ${vboxepack}",
+    command => "/usr/bin/wget http://download.virtualbox.org/virtualbox/${::virtualbox_version}/${vboxepahgck} && \
+                /usr/bin/VBoxManage extpack install --replace ${vboxepack} --accept-license=${license} \
+		&& /bin/rm -f ${vboxepack}",
     cwd     => '/tmp',
     unless  => "/usr/bin/VBoxManage list extpacks | /bin/grep 'Revision.*${::virtualbox_build}' > /dev/null 2>&1",
     notify  => Exec['stop_vboxweb'],
