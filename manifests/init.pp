9e3034e3236f01a8e55102564c3c39f5849d16d3
Jakov Sosic <jsosic@gmail.com>
2017-08-11 17:32:25 +0200

    Refactor module
    Simplify module by removing init scripts, templates for config files,
    user and only manage package and extensionpack.
    
    Everything else will be added later as needed.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,11 +4,9 @@
 # This module manages VirtualBox
 #
 class virtualbox (
-  $major        = '5.0',
-  $vboxweb_user = 'vbox',
-  $vboxweb_host = '127.0.0.10',
+  $version = '5.1',
+  $license = '_please_create_license_',
 ) {
-  require admintools::buildchain
 
   File {
     ensure => file,
@@ -17,69 +15,20 @@ class virtualbox (
     mode   => '0644',
   }
 
-  package { 'virtualbox':
+  package { 'VirtualBox':
     ensure => present,
-    alias  => 'VirtualBox',
-    name   => "VirtualBox-${major}",
+    name   => "VirtualBox-${version}",
   }
 
-  if $vboxweb_user == 'vbox' {
-    include virtualbox::user
-  }
+  $vboxepack="Oracle_VM_VirtualBox_Extension_Pack-${::virtualbox_version}-${::virtualbox_build}.vbox-extpack"
 
-  file { '/etc/init.d/vboxdrv':
-    mode    => '0755',
-    source  => 'puppet:///modules/virtualbox/vboxdrv',
-    require => Package['virtualbox'],
-  }
-
-  file { '/etc/init.d/vboxweb-service':
-    mode    => '0755',
-    source  => 'puppet:///modules/virtualbox/vboxweb-service',
-    require => Package['virtualbox'],
-  }
-
-  file { '/etc/sysconfig/virtualbox':
-    content => template('virtualbox/virtualbox.erb'),
-  }
-  file { '/etc/vbox/vbox.cfg':
-    ensure  => link,
-    target  => '/etc/sysconfig/virtualbox',
-    require => Package['virtualbox'],
-  }
-
-  exec { 'vboxdrv_setup':
-    command => '/etc/init.d/vboxdrv setup',
-    unless  => '/etc/init.d/vboxdrv status',
-    require => [
-      File['/etc/init.d/vboxdrv'],
-      File['/etc/vbox/vbox.cfg'],
-    ],
-  }
-
-  service { 'vboxdrv':
-    ensure  => running,
-    enable  => true,
-    require => [
-      File['/etc/init.d/vboxdrv'],
-      Exec['vboxdrv_setup'],
-    ],
-  }
-
-  file { '/var/log/vbox':
-    ensure => directory,
-    owner  => $vboxweb_user,
-    group  => root,
-  }
-
-  service { 'vboxweb-service':
-    ensure  => running,
-    enable  => true,
-    require => [
-      File['/var/log/vbox'],
-      File['/etc/vbox/vbox.cfg'],
-      File['/etc/init.d/vboxweb-service'],
-    ],
+  exec { 'install_vbox_extensionpack':
+    command => "/usr/bin/wget http://download.virtualbox.org/virtualbox/${::virtualbox_version}/${vboxepack} && \
+                /usr/bin/VBoxManage extpack install --replace ${vboxepack} --accept-license=${license} \
+		&& /bin/rm -f ${vboxepack}",
+    cwd     => '/tmp',
+    unless  => "/usr/bin/VBoxManage list extpacks | /bin/grep 'Revision.*${::virtualbox_build}' > /dev/null 2>&1",
+    require => Package['VirtualBox'],
   }
 
 }
